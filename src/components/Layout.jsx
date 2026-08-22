cfb3cbb7caf597072c18a5d0f1ba43c253c43c75
2872022b-coder <2872022b@gmail.com>
2026-08-23 01:34:00 +0300

    Delete src directory

--- src/components/Layout.jsx
+++
@@ -1,1 +0,0 @@
-import React from 'react';;import { Outlet } from 'react-router-dom';;import { Header } from './Header.jsx';;export function Layout() {  return (    <div className='min-h-screen bg-slate-50'>      <Header />      <main className='max-w-5xl mx-auto px-4 py-6'>        <Outlet />      </main>    </div>  );}
